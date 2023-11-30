0e7d89fcc55ac0fc67b22a5b4134c780a0c2285d
Marcos Dominguez <mrds0690@gmail.com>
2023-11-30 19:47:24 -0300

    feat: remove test outputs

--- PDS_Test.srcs/sim_1/new/tb_fir_filter.vhd
+++ PDS_Test.srcs/sim_1/new/tb_fir_filter.vhd
@@ -17,10 +17,8 @@
 -- Additional Comments:
 -- 
 ----------------------------------------------------------------------------------
-
-
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_1164.all;
 use IEEE.numeric_std.all;
 
 -- Uncomment the following library declaration if using
@@ -33,82 +31,80 @@ use IEEE.numeric_std.all;
 --use UNISIM.VComponents.all;
 
 entity tb_fir_filter is
---  Port ( );
+  --  Port ( );
 end tb_fir_filter;
 
 architecture arch of tb_fir_filter is
-    component fir_core is
-      generic (
-        N : positive := 4   -- Tamaño configurable
-      );
-      Port (
-        clk : in STD_LOGIC;           -- Reloj
-        rst : in STD_LOGIC;           -- Reset asincrónico
-        entrada : in STD_LOGIC_VECTOR(N-1 downto 0);        -- Señal de entrada
-        sumando : in STD_LOGIC_VECTOR(N-1 downto 0);        -- Sumando
-        coeficiente : in STD_LOGIC_VECTOR(N-1 downto 0);    -- Coeficiente
-        salida : out STD_LOGIC_VECTOR(N-1 downto 0);        -- Salida del componente
-        salida_sin_delay : out STD_LOGIC_VECTOR(N-1 downto 0);      -- Salida del componente
-        salida_mul : out STD_LOGIC_VECTOR(2 * N - 1 downto 0);
-        salida_sum : out STD_LOGIC_VECTOR(N - 1 downto 0)
-      );
-end component;
+  component fir_core is
+    generic
+    (
+      N : positive := 4 -- Tamaño configurable
+    );
+    port
+    (
+      clk              : in std_logic; -- Reloj
+      rst              : in std_logic; -- Reset asincrónico
+      entrada          : in std_logic_vector(N - 1 downto 0); -- Señal de entrada
+      sumando          : in std_logic_vector(N - 1 downto 0); -- Sumando
+      coeficiente      : in std_logic_vector(N - 1 downto 0); -- Coeficiente
+      salida           : out std_logic_vector(N - 1 downto 0); -- Salida del componente
+      salida_sin_delay : out std_logic_vector(N - 1 downto 0) -- Salida del componente
+    );
+  end component;
 
-constant N_tb : natural := 4;
+  constant N_tb : natural := 4;
 
-signal clk_s    : STD_LOGIC := '0';
-signal rst_s    : STD_LOGIC;
-signal coef_s   : STD_LOGIC_VECTOR(N_tb-1 downto 0);
-signal in_s     : STD_LOGIC_VECTOR(N_tb-1 downto 0);
-signal sum_s    : STD_LOGIC_VECTOR(N_tb-1 downto 0);
-signal outd_s   : STD_LOGIC_VECTOR(N_tb-1 downto 0);
-signal outs_s   : STD_LOGIC_VECTOR(N_tb-1 downto 0);
-signal outmul_s   : STD_LOGIC_VECTOR(2*N_tb-1 downto 0);
-signal outsum_s   : STD_LOGIC_VECTOR(N_tb-1 downto 0);
-
-
+  signal clk_s  : std_logic := '0';
+  signal rst_s  : std_logic;
+  signal coef_s : std_logic_vector(N_tb - 1 downto 0);
+  signal in_s   : std_logic_vector(N_tb - 1 downto 0);
+  signal sum_s  : std_logic_vector(N_tb - 1 downto 0);
+  signal outd_s : std_logic_vector(N_tb - 1 downto 0);
+  signal outs_s : std_logic_vector(N_tb - 1 downto 0);
 begin
 
-clk_s <= not clk_s after 10ns;
+  clk_s <= not clk_s after 10 ns;
 
-DUT: fir_core 
+  DUT : fir_core
 
-generic map(
-    N => N_tb
-)
+  generic
+  map(
+  N => N_tb
+  )
 
-port map(
-    clk                 => clk_s,
-    rst                 => rst_s,
-    entrada             => in_s,
-    sumando             => sum_s,
-    coeficiente         => coef_s,
-    salida              => outd_s,
-    salida_sin_delay    => outs_s,
-    salida_mul          => outmul_s,
-    salida_sum          => outsum_s
-);
+  port map
+  (
+    clk              => clk_s,
+    rst              => rst_s,
+    entrada          => in_s,
+    sumando          => sum_s,
+    coeficiente      => coef_s,
+    salida           => outd_s,
+    salida_sin_delay => outs_s
+    -- salida_mul          => outmul_s,
+    -- salida_sum          => outsum_s
+  );
 
-STIMULI:
-process
-begin
-report("Starting simulation");
-coef_s <=  std_logic_vector(to_signed (8, N_tb));
-sum_s  <=  std_logic_vector(to_signed (2, N_tb));
-rst_s <= '1';
-for i in 0 to 5 loop
-    wait until rising_edge(clk_s);
-end loop;
-rst_s <= '0';
+  STIMULI :
+  process
+  begin
+    report("Starting simulation");
+    coef_s <= std_logic_vector(to_signed (9, N_tb));
+    sum_s  <= std_logic_vector(to_signed (2, N_tb));
+    rst_s  <= '1';
+    for i in 0 to 5 loop
+      wait until rising_edge(clk_s);
+    end loop;
+    rst_s <= '0';
 
-for i in 1 to 15 loop
-    in_s <= std_logic_vector(to_unsigned(i,N_tb));
-    if (i > 9) then
+    for i in 1 to 15 loop
+      in_s <= std_logic_vector(to_unsigned(i, N_tb));
+      if (i > 9) then
         rst_s <= '1';
-    end if;
-    wait until rising_edge(clk_s);
-end loop;
+      end if;
+      wait until rising_edge(clk_s);
+    end loop;
 
-end process;
+  end process;
 
 end arch;
--- PDS_Test.srcs/sources_1/new/fir_filter.vhd
+++ PDS_Test.srcs/sources_1/new/fir_filter.vhd
@@ -1,42 +1,39 @@
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
---use IEEE.STD_LOGIC_ARITH.ALL;
---use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.STD_LOGIC_1164.all;
 use IEEE.numeric_std.all;
 
 entity fir_core is
-  generic (
-    N : positive := 2   -- Tamaño configurable
+  generic
+  (
+    N : positive := 2 -- Tamaño configurable
   );
-  Port (
-    clk : in STD_LOGIC;           -- Reloj
-    rst : in STD_LOGIC;           -- Reset asincrónico
-    entrada : in STD_LOGIC_VECTOR(N-1 downto 0);     -- Señal de entrada
-    sumando : in STD_LOGIC_VECTOR(N-1 downto 0);     -- Sumando
-    coeficiente : in STD_LOGIC_VECTOR(N-1 downto 0); -- Coeficiente
-    salida : out STD_LOGIC_VECTOR(N-1 downto 0);      -- Salida del componente
-    salida_sin_delay : out STD_LOGIC_VECTOR(N-1 downto 0);      -- Salida del componente
-    salida_mul : out STD_LOGIC_VECTOR(2 * N - 1 downto 0);
-    salida_sum : out STD_LOGIC_VECTOR(N - 1 downto 0)
+  port
+  (
+    clk              : in std_logic; -- Reloj
+    rst              : in std_logic; -- Reset asincrónico
+    entrada          : in std_logic_vector(N - 1 downto 0); -- Señal de entrada
+    sumando          : in std_logic_vector(N - 1 downto 0); -- Sumando
+    coeficiente      : in std_logic_vector(N - 1 downto 0); -- Coeficiente
+    salida           : out std_logic_vector(N - 1 downto 0); -- Salida del componente
+    salida_sin_delay : out std_logic_vector(N - 1 downto 0) -- Salida del componente
   );
 end fir_core;
 
 architecture Behavioral of fir_core is
-  signal multiplicacion1 : STD_LOGIC_VECTOR(2 * N - 1 downto 0);
-  signal suma : STD_LOGIC_VECTOR(N - 1 downto 0);
-  signal resultado : STD_LOGIC_VECTOR(N-1 downto 0);
-  signal flipflop_salida : STD_LOGIC_VECTOR(N-1 downto 0) := (others => '0');
+  signal multiplicacion1 : std_logic_vector(2 * N - 1 downto 0);
+  signal suma            : std_logic_vector(N - 1 downto 0);
+  signal resultado       : std_logic_vector(N - 1 downto 0);
+  signal flipflop_salida : std_logic_vector(N - 1 downto 0) := (others => '0');
 begin
   -- Multiplicación 1
-  multiplicacion1 <= STD_LOGIC_VECTOR(SIGNED(entrada) * SIGNED(coeficiente));
-  salida_mul <= multiplicacion1;
-  -- Multiplicación 2
-  suma <= STD_LOGIC_VECTOR(SIGNED(multiplicacion1(2*N - 2 downto N-1)) + SIGNED(sumando));
-  salida_sum <= suma;
-  -- Resultado
+  multiplicacion1 <= std_logic_vector(SIGNED(entrada) * SIGNED(coeficiente) + TO_SIGNED(2 ** (N - 2), 2 * N));
 
-  resultado <= (others => '0') when rst = '1' else suma;
-  
+  -- Multiplicación 2
+  suma <= std_logic_vector(SIGNED(multiplicacion1(2 * N - 2 downto N - 1)) + SIGNED(sumando));
+
+  resultado <= (others => '0') when rst = '1' else
+    suma;
+
   salida_sin_delay <= resultado;
 
   -- Flip-flop para almacenar el resultado
@@ -46,7 +43,7 @@ begin
       flipflop_salida <= resultado;
     end if;
   end process;
-  
+
   -- Salida del componente
   salida <= flipflop_salida;
 

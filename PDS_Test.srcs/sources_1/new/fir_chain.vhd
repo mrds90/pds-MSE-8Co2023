9e1ceaec4294a0cdc38551016b25b3ac6dad659b
Lucas Monzon Languasco <monzonlucasfabricio@gmail.com>
2023-12-02 16:49:12 -0300

    FIR CHAIN done. Automatic resize done.

--- PDS_Test.srcs/sources_1/new/fir_chain.vhd
+++ PDS_Test.srcs/sources_1/new/fir_chain.vhd
@@ -1,49 +1,55 @@
+--library ieee;
+--use ieee.std_logic_1164.all;
+--use ieee.numeric_std.all;
+
+--package bus_multiplexer_pkg is
+--    constant N : natural := 4;
+--    type bus_array is array(natural range <>) of std_logic_vector(N to 0);
+--end package;
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
-
-package bus_multiplexer_pkg is
-    constant N : natural := 4;
-    type bus_array is array(natural range <>) of std_logic_vector(N to 0);
-end package;
-
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-use work.bus_multiplexer_pkg.all;
+--use work.bus_multiplexer_pkg.all;
 
 entity filtro_fir_cadena is
   generic
   (
-    N : positive := 2; -- Tamaño configurable
-    M : positive := 3  -- Número de componentes FIR en la cadena
+    N : positive := 16; -- Tamaño configurable
+    M : positive := 4  -- Número de componentes FIR en la cadena
   );
   port
   (
     clk     : in STD_LOGIC; -- Reloj
     rst     : in STD_LOGIC; -- Reset asincrónico
     entrada : in STD_LOGIC_VECTOR(N - 1 downto 0); -- Señal de entrada común
-    coeficientes : in bus_array(0 to M-1);
+    coeficientes : in STD_LOGIC_VECTOR(N*M -1 downto 0);
     salida  : out STD_LOGIC_VECTOR(N - 1 downto 0) -- Salida del componente TOP
   );
 end filtro_fir_cadena;
 
 architecture Behavioral of filtro_fir_cadena is
   
-  signal sumando_intermedio : std_logic_vector(N - 1 downto 0) := (others => '0');
-  signal salida_final: std_logic_vector(N - 1 downto 0) := (others => '0');
+  
+  signal sumando_intermedio : STD_LOGIC_VECTOR(N*(M+1)-1 downto 0) := (others => '0');
+  signal salida_sin_delay_s : STD_LOGIC_VECTOR(N*(M+1)-1 downto 0) := (others => '0');
+  signal salida_final:        STD_LOGIC_VECTOR(N*M -1 downto 0)    := (others => '0');
 
   -- Declarar instancias de los componentes FIR
   component fir_core
+    generic
+    (
+       K : positive := N -- Tamaño configurable
+    ); 
     port
     (
       clk              : in std_logic;
       rst              : in std_logic;
-      entrada          : in std_logic_vector(N - 1 downto 0);
-      sumando          : in std_logic_vector(N - 1 downto 0);
-      coeficiente      : in std_logic_vector(N - 1 downto 0);
-      salida           : out std_logic_vector(N - 1 downto 0);
-      salida_sin_delay : out std_logic_vector(N - 1 downto 0)
+      entrada          : in std_logic_vector(K - 1 downto 0);
+      sumando          : in std_logic_vector(K - 1 downto 0);
+      coeficiente      : in std_logic_vector(K - 1 downto 0);
+      salida           : out std_logic_vector(K - 1 downto 0);
+      salida_sin_delay : out std_logic_vector(K - 1 downto 0)
     );
   end component;
 
@@ -58,14 +64,14 @@ begin
         clk              => clk,
         rst              => rst,
         entrada          => entrada,
-        sumando          => sumando_intermedio,
-        coeficiente      => coeficientes(i),
-        salida           => sumando_intermedio,
-        salida_sin_delay => salida_final
+        sumando          => sumando_intermedio((((i+1)*(N))-1) downto i*(N)),
+        coeficiente      => coeficientes((((i+1)*(N))-1) downto i*(N)),
+        salida           => sumando_intermedio((((i+2)*(N))-1) downto (i+1)*(N)),
+        salida_sin_delay => salida_final((((i+1)*(N))-1) downto i*(N))
       );
   end generate;
 
   -- Salida del componente TOP
-  salida <= salida_final;
+  salida <= salida_final(N*M-1 downto N*(M-1));
 
 end Behavioral;
--- PDS_Test.srcs/sources_1/new/fir_filter.vhd
+++ PDS_Test.srcs/sources_1/new/fir_filter.vhd
@@ -6,31 +6,31 @@ use IEEE.numeric_std.all;
 entity fir_core is
   generic
   (
-    N : positive := 2 -- Tamaño configurable
+    K : positive := 8 -- Tamaño configurable
   );
   port
   (
     clk              : in std_logic; -- Reloj
     rst              : in std_logic; -- Reset asincrónico
-    entrada          : in std_logic_vector(N - 1 downto 0); -- Señal de entrada
-    sumando          : in std_logic_vector(N - 1 downto 0); -- Sumando
-    coeficiente      : in std_logic_vector(N - 1 downto 0); -- Coeficiente
-    salida           : out std_logic_vector(N - 1 downto 0); -- Salida del componente
-    salida_sin_delay : out std_logic_vector(N - 1 downto 0) -- Salida del componente
+    entrada          : in std_logic_vector(K - 1 downto 0); -- Señal de entrada
+    sumando          : in std_logic_vector(K - 1 downto 0); -- Sumando
+    coeficiente      : in std_logic_vector(K - 1 downto 0); -- Coeficiente
+    salida           : out std_logic_vector(K - 1 downto 0); -- Salida del componente
+    salida_sin_delay : out std_logic_vector(K - 1 downto 0) -- Salida del componente
   );
 end fir_core;
 
 architecture Behavioral of fir_core is
-  signal multiplicacion1 : std_logic_vector(2 * N - 1 downto 0);
-  signal suma            : std_logic_vector(N - 1 downto 0);
-  signal resultado       : std_logic_vector(N - 1 downto 0);
-  signal flipflop_salida : std_logic_vector(N - 1 downto 0) := (others => '0');
+  signal multiplicacion1 : std_logic_vector(2 * K - 1 downto 0);
+  signal suma            : std_logic_vector(K - 1 downto 0);
+  signal resultado       : std_logic_vector(K - 1 downto 0);
+  signal flipflop_salida : std_logic_vector(K - 1 downto 0) := (others => '0');
 begin
   -- Multiplicación 1
-  multiplicacion1 <= std_logic_vector(SIGNED(entrada) * SIGNED(coeficiente) + TO_SIGNED(2 ** (N - 2), 2 * N));
+  multiplicacion1 <= std_logic_vector(SIGNED(entrada) * SIGNED(coeficiente) + TO_SIGNED(2 ** (K - 2), 2 * K));
 
   -- Multiplicación 2
-  suma <= std_logic_vector(SIGNED(multiplicacion1(2 * N - 2 downto N - 1)) + SIGNED(sumando));
+  suma <= std_logic_vector(SIGNED(multiplicacion1(2 * K - 2 downto K - 1)) + SIGNED(sumando));
 
   resultado <= (others => '0') when rst = '1' else
     suma;
